9e1885bc32b79f9ac6b20d7570ec73912f0b5018
Aivant Goyal <aivant.developer@gmail.com>
2018-07-08 20:08:34 -0500

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -2,14 +2,7 @@ const firebase = require("firebase");
 require("firebase/firestore");
 
 //Initializing firebase firestore
-firebase.initializeApp({
-    apiKey: "AIzaSyBxwatEUwIu9RmEBT52Jt2GyqRU98Y1KjQ",
-    authDomain: "exeter-dining-halls.firebaseapp.com",
-    databaseURL: "https://exeter-dining-halls.firebaseio.com",
-    projectId: "exeter-dining-halls",
-    storageBucket: "exeter-dining-halls.appspot.com",
-    messagingSenderId: "155953516475"
-});
+firebase.initializeApp("< YOUR FIREBASE INITIALIZATION INFO HERE >");
 
 const firestore = firebase.firestore();
 const settings = {
